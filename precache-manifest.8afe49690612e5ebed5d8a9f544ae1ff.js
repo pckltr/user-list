4071b715e5ff8e406469c7d473127d2280608764
pckltr <vlad.rusanescu@gmail.com>
2019-09-11 00:47:56 +0200

    Updates

---
+++ precache-manifest.8afe49690612e5ebed5d8a9f544ae1ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0f941cd1e5e66b9e86e49f4e3dd992",
+    "url": "/user-list/index.html"
+  },
+  {
+    "revision": "feb7dfe0ba37b3f83622",
+    "url": "/user-list/static/css/main.4f54848f.chunk.css"
+  },
+  {
+    "revision": "a969b7cac51005448c46",
+    "url": "/user-list/static/js/2.a76dca18.chunk.js"
+  },
+  {
+    "revision": "feb7dfe0ba37b3f83622",
+    "url": "/user-list/static/js/main.60910947.chunk.js"
+  },
+  {
+    "revision": "9a30e52de5ab86c27836",
+    "url": "/user-list/static/js/runtime~main.6f1f7666.js"
+  }
+]);
